8a0a7798445544a74dc41ad1944ee9f845bf7381
Prasad_Kunapareddy <84506302+prasadkunapareddy@users.noreply.github.com>
2022-06-09 19:32:42 +0530

    Add files via upload

---
+++ Alphabet.py
@@ -0,0 +1,15 @@
+x=str(input())
+z=0
+y={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+for i in range(len(x)):
+    z=z+1
+print(z)
+
+
+# x=int(input("Enter"))
+# y=[int(i) for i in input().split("")]
+# z=int(input("Enter"))
+# a=[(j) for i in input().split("")]
+# for i in range(x):
+#     print(y[i],end="")
+#     print(a[i],end="")
